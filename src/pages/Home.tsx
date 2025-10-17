e09f6758a28ae3af100f589325e7c894dd1799df
alinw <alinwinskingsleigh@snu.ac.kr>
2025-10-17 13:13:39 +0900

    fix/css_기본 화면 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles.css";
 
 interface HomeProps {
   user: { name: string } | null;
@@ -7,7 +8,13 @@ interface HomeProps {
 const Home: React.FC<HomeProps> = ({ user }) => {
   return (
     <div className="home">
-      {user ? <h2>안녕하세요, {user.name}님 👋</h2> : <h2>로그인 후 이용해주세요.</h2>}
+      <div className="center-box">
+        {user ? (
+          <h2>안녕하세요, {user.name}님 👋</h2>
+        ) : (
+          <h2>로그인 후 이용해주세요.</h2>
+        )}
+      </div>
     </div>
   );
 };
