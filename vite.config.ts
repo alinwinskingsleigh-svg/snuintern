66d34386675328b8eb56faf10f4c014d4dfefaca
alinw <alinwinskingsleigh@snu.ac.kr>
2025-11-09 21:08:48 +0900

    프록시 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api-internhasha.wafflestudio.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
